d2fd4e245b971a0e3c45b85f6c6542f911a22e23
herbertUG <herbertoketayot@gmail.com>
2025-01-06 01:30:14 +0300

    using state set up sidebar

--- src/app/(components)/Navbar/index.tsx
+++ src/app/(components)/Navbar/index.tsx
@@ -1,14 +1,24 @@
 "use client";
 import { Bell, Menu, Settings, Sun } from "lucide-react";
 import Link from "next/link";
+import { useAppDispatch, useAppSelector } from "../../../../redux";
+import { setIsSidebarCollapsed } from "@/state";
 
 const Navbar = () => {
+  const dispatch = useAppDispatch();
+    const isSidebarCollapsed = useAppSelector(
+      (state) => state.global.isSidebarCollapsed
+    );
+  
+    const toggleSidebar = () => {
+      dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
+    }
   return (
     <div className="flex justify-between items-center w-full mb-7">
       <div className="flex justify-between items-center gap-5">
         <button
           className="px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100"
-          onClick={() => console.log("clicked")}
+          onClick={toggleSidebar}
         >
           <Menu className="w-4 h-4" />
         </button>
--- src/app/(components)/Sidebar/index.tsx
+++ src/app/(components)/Sidebar/index.tsx
@@ -1,27 +1,53 @@
 "use client";
 import { Menu } from "lucide-react";
+import { useAppDispatch, useAppSelector } from "../../../../redux";
+import { setIsSidebarCollapsed } from "@/state";
 
 const Sidebar = () => {
+  const dispatch = useAppDispatch();
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed
+  );
+
+  const toggleSidebar = () => {
+    dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
+  };
+
+  const sidebarClassNames = `fixed flex flex-col ${
+    isSidebarCollapsed ? "w-0 md:w-16" : "w-72 md:w-64"
+  } bg-white transition-all duration-300 h-full z-40`;
+
   return (
-    <div>
-      {/*  */}
-      <div className="flex gap-3 justify-between md:justify-normal items-center pt-8">
+    <div className={sidebarClassNames}>
+      {/* top logo */}
+      <div
+        className={`flex gap-3 justify-between md:justify-normal items-center pt-8 ${
+          isSidebarCollapsed ? "px-5" : "px-8"
+        }`}
+      >
         <div>Logo</div>
-        <h1 className="font-extrabold">STOCK</h1>
-        <button className="md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100">
+        <h1
+          className={`${
+            isSidebarCollapsed ? "hidden" : "block"
+          } font-extrabold text-2xl`}
+        >
+          STOCK
+        </h1>
+        <button
+          className="md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100"
+          onClick={toggleSidebar}
+        >
           <Menu className="w-4 h-4" />
         </button>
       </div>
 
-        {/* links */}
-        <div className="flex-grow mt-8">
+      {/* links */}
+      <div className="flex-grow mt-8"></div>
 
-        </div>
-
-        {/* footer */}
-        <div className="text-center text-xs text-gray-500">
-            &copy; 2021 Stock. All rights reserved.
-        </div>
+      {/* footer */}
+      <div className="text-center text-xs text-gray-500">
+        &copy; 2021 Stock. All rights reserved.
+      </div>
     </div>
   );
 };
